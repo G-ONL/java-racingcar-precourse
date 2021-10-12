1c7a22bab516178eff0c1da224e99c28001b2093
je941004 <je941004@naver.com>
2021-10-12 12:24:35 +0900

    refactor: 모두 삭제하고 다시 시작하기

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,9 +1,6 @@
 package racinggame;
 
-import racinggame.controller.RacingCarGameController;
-
 public class Application {
     public static void main(String[] args) {
-        RacingCarGameController.start();
     }
 }
--- src/main/java/racinggame/utils/GameConstant.java
+++
@@ -1,8 +0,0 @@
-package racinggame.utils;
-
-public final class GameConstant {
-    public static final int MOVING_MIN = 4;
-    public static final int MOVING_MAX = 9;
-    public static final int STOP_MIN = 0;
-    public static final int STOP_MAX = 3;
-}
